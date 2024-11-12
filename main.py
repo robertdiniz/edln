f0c6d9e3e5d6ac9aacc052ed1c8aff24f4ced476
robert <dinizrobert2002@gmail.com>
2024-11-11 21:53:56 -0300

    finish

---
+++ main.py
@@ -0,0 +1,134 @@
+from min_heap import MinHeap
+
+# pai = (index - 1 ) // 2
+# filho esquerda = 2 * index + 1
+# filho direita = 2 * index + 2
+
+
+
+if __name__ == "__main__":
+    atv1 = MinHeap()
+    # inserindo os dados da q1
+    print('Construindo o HEAP:')
+    atv1.insert(10)
+    atv1.insert(5)
+    atv1.insert(20)
+    atv1.insert(1)
+    atv1.insert(15)
+    atv1.insert(30)
+    atv1.insert(25)
+
+    # alterando prioridades
+    print('Alterando Prioridades')
+    atv1.change_priority(3, 50)
+    atv1.change_priority(1, 8)
+
+    # removendo os roots
+    atv1.remover_raiz()
+    atv1.remover_raiz()
+    atv1.remover_raiz()
+
+    # selecionando alta prioridade
+    print(f'Elemento de alta prioridade: {atv1.get_high_priority()}')
+
+    # mostrando heap ordenado
+    print('Mostrando o heap ordenado')
+    heap_atv1_sort = atv1.heap_sort()
+    print(heap_atv1_sort)
+
+
+    # q2 - ordem crescente
+    atv2 = MinHeap()
+    atv2.insert(1)
+    atv2.insert(2)
+    atv2.insert(3)
+    atv2.insert(4)
+    atv2.insert(5)
+    atv2.insert(6)
+    atv2.insert(7)
+    atv2.insert(8)
+    atv2.insert(9)
+    atv2.insert(10)
+
+    # alterando prioridades
+    print('Q2 - Alterando prioridades')
+    atv2.change_priority(4, 15)
+    atv2.change_priority(8, 3)
+
+    # selecionando alta prioridade
+    print(f'Elemento de alta prioridade: {atv2.get_high_priority()}')
+
+    # removendo
+    print('Removendo roots:')
+    atv2.remover_raiz()
+    atv2.remover_raiz()
+    atv2.remover_raiz()
+    atv2.remover_raiz()
+    atv2.remover_raiz()
+
+    # heap sort
+    print('Heap Sort:')
+    heap_atv2_sort = atv2.heap_sort()
+    print(heap_atv2_sort)
+
+    # Q3 - Sequência Decrescente
+    atv3 = MinHeap()
+
+    print('Q3 - Inserindo dados no heap')
+    atv3.insert(50)
+    atv3.insert(40)
+    atv3.insert(30)
+    atv3.insert(20)
+    atv3.insert(10)
+    atv3.insert(5)
+    atv3.insert(3)
+
+    print('Alterando prioridades')
+    atv3.change_priority(2, 60)
+    atv3.change_priority(5, 1)
+
+    # selecionando alta prioridade
+    print(f'Elemento de alta prioridade: {atv3.get_high_priority()}')
+
+    print('Removendo roots')
+    atv3.remover_raiz()
+    atv3.remover_raiz()
+    atv3.remover_raiz()
+
+    print('Heap Sort:')
+    atv3_heap_sort = atv3.heap_sort()
+    print(atv3_heap_sort)
+
+    # Q4 - Dados Aleatórios Maiores
+    atv4 = MinHeap()
+
+    atv4.insert(13)
+    atv4.insert(26)
+    atv4.insert(19)
+    atv4.insert(17)
+    atv4.insert(24)
+    atv4.insert(31)
+    atv4.insert(22)
+    atv4.insert(11)
+    atv4.insert(8)
+    atv4.insert(20)
+    atv4.insert(5)
+    atv4.insert(27)
+    atv4.insert(18)
+
+    print('Alterando prioridades')
+    atv4.change_priority(7, 35)
+    atv4.change_priority(10, 12)
+
+    # selecionando alta prioridade
+    print(f'Elemento de alta prioridade: {atv4.get_high_priority()}')
+
+    print('Removendo roots')
+    atv4.remover_raiz()
+    atv4.remover_raiz()
+    atv4.remover_raiz()
+    atv4.remover_raiz()
+
+    print('Heap Sort:')
+    atv4_heap_sort = atv4.heap_sort()
+    print(atv4_heap_sort)
--- min_heap.py
+++ min_heap.py
@@ -14,9 +14,10 @@ class MinHeap:
 
     def subir(self, index):
         while index > 0:
-            no_pai = (index - 1) // 2
+            no_pai = (index - 1) // 2 # [3] = 4
             if self.heap[index] < self.heap[no_pai]:
                 self.heap[index], self.heap[no_pai] = self.heap[no_pai], self.heap[index]
+                self.display_heap()
                 index = no_pai
             else:
                 break
@@ -25,40 +26,67 @@ class MinHeap:
         if len(self.heap) == 0:
             return None
         
-        valor_raiz = self.heap[0] # 3
-        self.heap[0] = self.heap.pop() # [0] = 6
-        self.descer(0) # index
+        valor_raiz = self.heap[0] 
+
+        if len(self.heap) > 1:
+            self.heap[0] = self.heap.pop() 
+            self.descer(0)
+        else:
+            self.heap.pop()
+
         self.display_heap()
         return valor_raiz
     
     def descer(self, index):
-        tamanho = len(self.heap) # 4
+        tamanho = len(self.heap)
 
-        while True: # 6 4 15 10
-            f_esquerda = 2 * index + 1 # 1
-            f_direita = 2 * index + 2 # 2
-            menor = index # 0
+        while True: # index = 4
+            f_esquerda = 2 * index + 1 
+            f_direita = 2 * index + 2 
+            menor = index # 1
 
-            if f_esquerda < tamanho and self.heap[f_esquerda] < self.heap[menor]: # 6 4 15 10
-                print(f'[{f_esquerda}] = {self.heap[f_esquerda]} é menor que [{menor}] = {self.heap[menor]}')
-                menor = f_esquerda
+            if f_esquerda < tamanho and self.heap[f_esquerda] < self.heap[menor]:
+                menor = f_esquerda # [3] = 10
             
             if f_direita < tamanho and self.heap[f_direita] < self.heap[menor]:
-                menor = f_direita
+                menor = f_direita # [4] = 15 < [3] = 10 
             
             if menor != index:
-                print(f'{menor} - {index}')
-                self.heap[index], self.heap[menor] = self.heap[menor], self.heap[index] # 4 6 15 10
+                self.heap[index], self.heap[menor] = self.heap[menor], self.heap[index] 
                 index = menor
+                self.display_heap()
             else:
-                print(f'{menor} - {index} agora é')
                 break
+
+    def change_priority(self, index, new_priority):
+        old_priority = self.heap[index] # [8] = 9
+        self.heap[index] = new_priority # 3
+
+        self.display_heap()
+
+        if new_priority < old_priority:
+            self.subir(index)
+        else:
+            self.descer(index)
+
 
     def display_heap(self):
         print(f'Heap atual: {self.heap}')
 
+    def get_high_priority(self):
+        return self.heap[0]
+    
+    def heap_sort(self):
 
-min_heap = MinHeap()
+        sorted_heap = []
+
+        while len(self.heap) > 0:
+            sorted_heap.append(self.remover_raiz())
+
+        return sorted_heap
+
+
+# min_heap = MinHeap()
 
 # dados que eu tava usando pra teste 👌
 # min_heap.insert(10)
@@ -67,15 +95,24 @@ min_heap = MinHeap()
 # min_heap.insert(6)
 # min_heap.insert(3)
 
-min_heap.insert(10)
-min_heap.insert(5)
-min_heap.insert(20)
-min_heap.insert(1)
-min_heap.insert(15)
-min_heap.insert(30)
-min_heap.insert(25)
+# min_heap.insert(10)
+# min_heap.insert(5)
+# min_heap.insert(20)
+# min_heap.insert(1)
+# min_heap.insert(15)
+# min_heap.insert(30)
+# min_heap.insert(25)
 
-print(min_heap.heap)
+# min_heap.change_priority(1, 50)
+# min_heap.change_priority(1, 8)
 
-min_value = min_heap.remover_raiz()
-print(min_value)
+
+# heap_ordenado = min_heap.heap_sort()
+# print(heap_ordenado)
+
+# min_heap.remover_raiz()
+# min_heap.remover_raiz()
+# min_heap.remover_raiz()
+
+# min_value = min_heap.remover_raiz()
+# print(min_value)
