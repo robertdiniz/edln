973dc06c7f3733f2fe049e8e2615c561e8f76dbe
robert <dinizrobert2002@gmail.com>
2024-12-10 00:41:45 -0300

    BST: deletion node + ordinances

--- arvores/binary_search_tree.py
+++ arvores/binary_search_tree.py
@@ -9,11 +9,13 @@ def insert(root, value):
     if value < root.data:
         if not root.left:
             root.left = Node(value)
-        insert(root.left, value)
+        else:
+            insert(root.left, value)
     if value > root.data:
         if not root.right:
             root.right = Node(value)
-        insert(root.right, value)
+        else:
+            insert(root.right, value)
     
 def search(value, current=None):
 
@@ -40,11 +42,6 @@ def count_leaves(node):
         return 1
     return count_leaves(node.left) + count_leaves(node.right)
 
-def get_successor(curr):
-    curr = curr.right
-    while curr is not None and curr.left is not None:
-        curr = curr.left
-    return curr
 
 def del_node(root, key):
 
@@ -57,16 +54,23 @@ def del_node(root, key):
         root.right = del_node(root.right, key)
 
     if key == root.data:
+        # caso 1 - o nó atual é uma folha
+        if root.left is None and root.right is None:
+            return None
+
+        # caso 2 - o nó possui filho esquerdo ou direito
         if root.left is None:
             return root.right
-
         if root.right is None:
             return root.left
 
-        succ = get_successor(root)
-        root.key = succ.key
-        root.right = del_node(root.right, succ.key)
-        
+        # caso 3 - possui 2 filhos, percorre o sucessor
+        current = root.right
+        while(current.left is not None):
+            current = current.left
+        root.data = current.data
+        root.right = del_node(root.right, current.data)
+
     return root
 
 
@@ -78,15 +82,58 @@ def size(node):
     return hleft + hright + 1
 
 
+def pre_order(node):
+    if node is None:
+        return
+    
+    print(node.data, end=" ")
+
+    pre_order(node.left)
+    pre_order(node.right)
+
+def post_order(node):
+    if node is None:
+        return
+    
+    post_order(node.left)
+    post_order(node.right)
+
+    print(node.data, end=" ")
+
+
 if __name__ == "__main__":
     root = None
     root = Node(10)
     insert(root, 5)
+    insert(root, 1)
     insert(root, 3)
-    insert(root, 6)
+    insert(root, 14)
+    insert(root, 12)
+    insert(root, 16)
     in_order(root)
     print()
-    print(search(5, root))
-    print(search(6, root))
-    print(size(root))
+    pre_order(root)
+    print()
+    post_order(root)
+    print()
+
+    # print(search(5, root))
+    # print(search(6, root))
+    # print(leaves(root))
+    # print(count_leaves(root))
+
+    print()
+
+    root = del_node(root, 5)
+    in_order(root)
+    print()
+
     print(count_leaves(root))
+    print(size(root))
+
+
+    # del_node(root, 3)
+    # in_order(root)
+    # print()
+    # del_node(root, 5)
+    # in_order(root)
